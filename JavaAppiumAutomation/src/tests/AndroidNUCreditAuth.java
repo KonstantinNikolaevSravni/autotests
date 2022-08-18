39862f112b205b3362fd9e81ef4a3c8183e66a15
Konstantin Nikolaev <konstantin.nikolaev@sravni.ru>
2022-08-18 16:45:13 +0300

    release v1.0.3

--- JavaAppiumAutomation/src/tests/AndroidNUCreditAuth.java
+++ JavaAppiumAutomation/src/tests/AndroidNUCreditAuth.java
@@ -78,23 +78,34 @@ public class AndroidNUCreditAuth extends CoreTestCase {
     {
         StartPageObject StartPageObject = new StartPageObject(driver);
 
-        MainPageObject.waitForElementPresent(By.xpath("//*[contains(@text,'Нажимая «Далее», я соглашаюсь')]"),"");
-        MainPageObject.waitForElementAndClick(By.xpath("//*[contains(@text,'с правилами предоставления информации')]"),"",5);
-        MainPageObject.waitForElementPresent(By.xpath("//*[contains(@text,'Согласие на обработку персональных данных')]"),"");
-        MainPageObject.waitForElementAndClick(By.xpath("//*[@content-desc='Navigate up']"),"",5);
-     //   StartPageObject.promoElementTitle("ОСАГО онлайн");
+        MainPageObject.waitForElementPresent(By.xpath("//*[contains(@text,'Нажимая «Далее», я соглашаюсь c правилами предоставления информации и согласием на обработку персональных данных')]"),"");
+
+        MainPageObject.waitForElementAndClick(By.xpath("//*[contains(@text,'правилами предоставления информации')]"),"",5);
+
+        MainPageObject.waitForElementPresent(By.xpath("//*[contains(@text,'Пользовательское соглашение сервиса Сравни.ру')]"),"",10);
+        driver.navigate().back();
+
+        MainPageObject.waitForElementAndClick(By.xpath("//*[contains(@text,'согласием на обработку персональных данных')]"),"",5);
+
+        MainPageObject.waitForElementPresent(By.xpath("//*[contains(@text,'СОГЛАСИЕ НА ОБРАБОТКУ ПЕРСОНАЛЬНЫХ ДАННЫХ')]"),"");
+
+
+
+
+        StartPageObject.promoScreen("ОСАГО онлайн", "Купите или продлите любой полис ОСАГО. Сохраним все скидки, без комиссии, с экономией до 3 500 ₽");
         StartPageObject.promoSwipe("//*[contains(@text,'ОСАГО онлайн')]");
         MainPageObject.waitForElementPresent(By.xpath("//*[contains(@text,'Все банки предоставляют разные условия, например, ставки от 5.99% до 30% годовых…')]"),"");
         MainPageObject.waitForElementAndClick(By.xpath("//*[contains(@text,'Подробнее')]"),"",5);
+
         MainPageObject.waitForElementPresent(By.xpath("//*[contains(@text,'Все банки предоставляют разные условия, например, ставки от 5.99% до 30% годовых, срок кредита от 6 месяцев до 15 лет. Пример расчета: по кредиту на сумму 350 000 руб. на 3 года под ставку 9% годовых общая сумма выплат составит 400 716 руб. с ежемесячным платежом 11 131 руб./мес. Штраф за просрочку ежемесячного платежа: 0,05% - 0,2% от суммы просроченной задолженности за каждый день нарушения обязательств. Не является публичной офертой.')]"),"");
         MainPageObject.waitForElementAndClick(By.xpath("//*[@content-desc='Navigate up']"),"",5);
-   //     StartPageObject.promoElementTitle("Персональный подбор кредита");
+        StartPageObject.promoScreen("Персональный подбор кредита", "Заполните информацию о себе, сравните персональные предложения и узнайте какой банк одобрит кредит или кредитную карту");
 
     }
 
     @Test
-    public void testLoginFullSwipe()
-    {
+    public void testLoginFullSwipe() throws InterruptedException {
+
         StartPageObject StartPageObject = new StartPageObject(driver);
 
         StartPageObject.promoSwipe("//*[contains(@text,'ОСАГО онлайн')]");
@@ -102,7 +113,7 @@ public class AndroidNUCreditAuth extends CoreTestCase {
         StartPageObject.promoSwipe("//*[contains(@text,'Кредитный рейтинг')]");
         StartPageObject.promoSwipe("//*[contains(@text,'Умный чат бот')]");
         StartPageObject.promoSwipe("//*[contains(@text,'И много других продуктов…')]");
-    //    StartPageObject.promoClickButton();
+        StartPageObject.promoButtonNextFinal();
 
         // Ввод номера
 
