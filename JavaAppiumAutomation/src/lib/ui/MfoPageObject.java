62871264a4395ca506a53f168aa36139bc6c1b4f
Konstantin Nikolaev <konstantin.nikolaev@sravni.ru>
2022-10-13 18:38:17 +0300

    “release”

---
+++ JavaAppiumAutomation/src/lib/ui/MfoPageObject.java
@@ -0,0 +1,13 @@
+package lib.ui;
+
+import io.appium.java_client.AppiumDriver;
+
+public class MfoPageObject extends MainPageObject{
+
+    public MfoPageObject(AppiumDriver driver)
+    {
+        super(driver);
+    }
+
+
+}
